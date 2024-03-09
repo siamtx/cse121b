30bd964febbd42de1a27b37a0f1cb48db4fbd40e
siamtx <siamtx@yahoo.com>
2024-03-08 21:37:16 -0500

    This is a major update.

---
+++ Readmeagain.md
@@ -0,0 +1,3 @@
+I am having trouble getting VSCode to commit to github.
+
+Will this work?
---
+++ scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Tim Howell";
+let currentYear = '2024';
+let profilePicture = 'images/Tim Howell.jpg';
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.getElementById('#year');
+const imageElement = document.querySelector('pic');
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
+
+dfbadsfhbsfg;
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
