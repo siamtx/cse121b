da58732c57b24f88e4a957dec082dc4d6269614e
siamtx <siamtx@yahoo.com>
2024-03-18 21:34:08 -0400

    SOMETHING THERE.

---
+++ week03/array_Methods.js
@@ -0,0 +1,26 @@
+// Week Three Check your Understanding
+
+// Using the 'array.filter()' create an array named namesB
+// from the 'names' array that start with B.
+
+// declare the array.
+let names = ['Namcy', 'Blessing', 'Jorge', 'Svetlana', 'Bob'];
+
+// array.filter()
+let namesB = names.filter(name => name.charAt(0) === 'B'); 
+
+// Using the array.map() method, create an array named 'namesLength'
+// that contains the length of each name.
+
+// array.map()
+let namesLength = names.map((name) => namesLength);
+
+// Using the 'names.reduce()' create an expression that
+// returns the average string length of the names in the 
+// names array.
+
+names.reduce((total, name) => total + names.length, 0) / names.length;
+
+names.filter;
+names.map;
+names.reduce;
