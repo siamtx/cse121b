cf6a4498c5d0a22898d732fe555f89faaaae67f3
siamtx <siamtx@yahoo.com>
2024-03-20 22:30:12 -0400

    working along, +/- is working

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -2,25 +2,43 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
-function add(number1, number2)
-{
-    return (number1 + number2);
+function add(number1, number2){
+    return number1 + number2;
 }
 
-function addNumbers(number1, number2)
-{
-    let addNumber1 = Number.document.querySelector();
-    number1 = document.getElementById("#add1");
-    number2 = document.getElementById("#add2");
-    document.querySelector('#sum').value = add(number1, number2);  
+function addNumbers(number1, number2){
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);  
 }
-document.querySelector('#addnumbers').addEventListener('click', addNumbers);
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 
 /* Function Expression - Subtract Numbers */
+function subtract(number1, number2){   
+    return(number1 - number2);
+}
 
+function subtractNumbers(number1, number2){
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+}
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
+function multiply(factor1, factor2)
+{
+    return (factor1 * factor2);
+}
 
+function multiplyNumbers(factor1, factor2)
+{
+    let multifact1 = Number(document.querySelector('#factor1').value);
+    let multifact2 = Number(document.querySelector('#factor2').value);
+    document.querySelector('#product').value = multiplyNumbers(multifact1, multifact2);
+}
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
 
