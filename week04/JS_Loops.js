660b15fdceb8f53fe438e4b0d2223a274bddd301
siamtx <siamtx@yahoo.com>
2024-03-22 19:14:57 -0400

    Week 4 folder

---
+++ week04/JS_Loops.js
@@ -0,0 +1,21 @@
+// JS_Loops.js
+myInfo = {
+    name: "Brother T",
+    photo: "images/photo.jpg",
+    favoriteFoods: ["Fettucini", "Steak", "Chicken", "Shrimp", "Baked Potato"],
+    hobbies: ["Reading", "Fishing", "Camping"],
+    placesLived: [
+      {
+        place: "Rexburg, ID",
+        length: "5 years",
+      },
+      {
+        place: "Ammon, ID",
+        length: "3 years",
+      },
+      {
+        place: "Sandy, UT",
+        length: "1 year",
+      },
+    ],
+  };
